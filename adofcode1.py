00ba2a3edead13e4b9b9c41da618e3dacc043384
younesko <119075955+younesko@users.noreply.github.com>
2022-12-02 20:58:50 +0100

    Update adofcode1.py

--- adofcode1.py
+++ adofcode1.py
@@ -1,6 +1,6 @@
 #advent of code day 1
 
-
+#solution of the second part
 def file2list(o):
     '''create a list of lists with elfs items'''
     inner, outer=[], []
@@ -19,21 +19,4 @@ o = open(filep, "r")
 print(sum(sorted(map(sum, file2list(o)))[-3:]))
 
 
-'''
-elfs= enumerate(file2list(o))
 
-calmax=0
-total=[]
-
-for x, y in elfs:
-    a=sum(y)
-    total.append(a)
-    if a> calmax:
-        calmax=a
-        index=x+1
-        
-print ("elf number ", sum(sorted(total)[-3:]))
-    
-o.close()
-    
-'''
