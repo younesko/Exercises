ee6c15b432cbfc3f7c291c95e3bbd557be30b0d5
younesko <119075955+younesko@users.noreply.github.com>
2022-12-02 20:57:46 +0100

    Add files via upload

---
+++ adofcode1.py
@@ -0,0 +1,39 @@
+#advent of code day 1
+
+
+def file2list(o):
+    '''create a list of lists with elfs items'''
+    inner, outer=[], []
+    for x in o:
+        try:
+            inner.append(int(x))
+        except:
+            outer.append(inner)
+            inner=[]
+    return outer
+
+
+filep= "C:\\Users\\gumis\\OneDrive\\Desktop\\input2.txt"
+o = open(filep, "r")
+
+print(sum(sorted(map(sum, file2list(o)))[-3:]))
+
+
+'''
+elfs= enumerate(file2list(o))
+
+calmax=0
+total=[]
+
+for x, y in elfs:
+    a=sum(y)
+    total.append(a)
+    if a> calmax:
+        calmax=a
+        index=x+1
+        
+print ("elf number ", sum(sorted(total)[-3:]))
+    
+o.close()
+    
+'''
