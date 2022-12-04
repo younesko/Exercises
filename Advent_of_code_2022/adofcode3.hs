16e2fa8f99fdbd4cab72c3044b1ffb4098cb11f8
younesko <119075955+younesko@users.noreply.github.com>
2022-12-04 01:39:25 +0100

    Update adofcode3.hs

--- Advent_of_code_2022/adofcode3.hs
+++ Advent_of_code_2022/adofcode3.hs
@@ -1,17 +1,26 @@
 
--- Part one day 3 Advent of code
+-- Part one and two Advent of code day 3
+import Data.List
 
--- divide first element in two
+
+-- divide a single rucksack in two
 first_p :: [Char] -> [Char]
 first_p s = take (div (length s )2) s 
 
 last_p :: [Char] -> [Char]
 last_p s = drop (div (length s )2) s
 
--- gives the element in common
+-- Element in common in the same rucksack
 
 comn :: [Char] -> Char
-comn s = filter (\x -> elem x (first_p s) ) (last_p s)!! 0
+comn s = nub (filter (\x -> elem x (first_p s) ) (last_p s))
+
+-- Element in common among the 3 elfs
+comn2 :: [[Char]] -> [Char]
+comn2 [] = []
+comn2 (_:[]) = []
+comn2 (_:_:[])=[]
+comn2 (x:y:z:xs)=  nub (filter (\j -> elem j z) (filter (\i -> elem i x) y)) ++ (comn2 xs)
 
 --create a string with all common letters and then evaluate them
 
@@ -25,8 +34,12 @@ f _ [] = 0
 f a ((alfa, beta):xs)
     | (a == alfa)  =  beta
     | otherwise = f a (xs)
+    
+mypath = "input" -- filepath
 
 main = do 
-    myinput <- readFile "mypath\\input.txt"
-    let rix = sum ( map (\x -> f x testc) (map  comn  (words myinput)) )
+    myinput <- readFile mypath
+    let rix1 = sum ( map (\x -> f x testc) (map  comn  (words myinput)) ) -- Solving the first problem
+    let rix2 = sum ( map (\x -> f x testc) (comn2 (words myinput))) -- Solving the second problem
+    print (rix)
     print (rix)
